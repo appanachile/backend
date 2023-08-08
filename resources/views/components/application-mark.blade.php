de66020cc84e7fcc9d869de886475e6cf1b2dc79
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-08 01:29:41 -0400

    u

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,4 +1,4 @@
-<figure class="flex">
+<figure class="flex space-x-2 items-center">
   <img src="https://tailus.io/sources/blocks/food-delivery/preview/images/icon.png" class="w-12" alt="tailus logo" width="144" height="133">
   <span class="text-2xl font-bold text-yellow-900">Appaña <span class="text-yellow-700">app</span></span>
 
--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,5 +1,5 @@
 <a href="/">
-    <figure>
+    <figure class="flex space-x-2 items-center">
         <img src="https://tailus.io/sources/blocks/food-delivery/preview/images/icon.png" class="w-12" alt="tailus logo" width="144" height="133">
         <span class="text-2xl font-bold text-yellow-900">Appaña <span class="text-yellow-700">app</span></span>
     
